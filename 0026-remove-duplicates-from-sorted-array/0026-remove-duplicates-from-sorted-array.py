93275c0f91d95c6a01c1b119f849897cebe30c4c
Yun Seok Min (YSM) <yunsukmin522@snu.ac.kr>
2026-02-03 23:37:07 +0900

    Time: 0 ms (100%), Space: 20.8 MB (9.76%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,8 +1,6 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         index = 0
-
-        
         for s in range(len(nums)):
             if(nums[index] != nums[s]):
                 index += 1
